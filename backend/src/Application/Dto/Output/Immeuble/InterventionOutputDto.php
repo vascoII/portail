51775ebbbdbe24a4735d3fb9f120e7b092e4e286
DA SILVA, Fabrice <fabrice.da-silva.extern@techem.fr>
2025-09-24 16:41:37 +0200

    Updated: backend Application

--- backend/src/Application/Dto/Output/Immeuble/InterventionOutputDto.php
+++ backend/src/Application/Dto/Output/Immeuble/InterventionOutputDto.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Application\Dto\Output\Immeuble;
 
-final class LinterventionOutputDto
+final class InterventionOutputDto
 {
   /** @param array<int, mixed> $data */
   public function __construct(public readonly array $data) {}
