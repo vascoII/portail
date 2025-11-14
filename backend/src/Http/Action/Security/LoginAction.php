43918d0e48475cde131853b8c45cbedcc255cd4f
DA SILVA, Fabrice <fabrice.da-silva.extern@techem.fr>
2025-11-14 10:06:10 +0100

    Updated: LoginAction

--- backend/src/Http/Action/Security/LoginAction.php
+++ backend/src/Http/Action/Security/LoginAction.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Action\Security;
 
-//use App\Application\Factory\Security\SecurityInputFactory;
-//use App\Application\UseCase\Security\LoginUseCase;
+use App\Application\Factory\Security\SecurityInputFactory;
+use App\Application\UseCase\Security\LoginUseCase;
 use App\Http\Action\AbstractAction;
 use App\Http\Action\ActionInterface;
 use App\Http\Responder\ResponderInterface;
@@ -22,41 +22,15 @@ final class LoginAction extends AbstractAction implements ActionInterface
 {
     public function __construct(
         private readonly ResponderInterface $responder,
-        //private readonly ?LoginUseCase $useCase = null,
-        //private readonly ?SecurityInputFactory $inputFactory = null
+        private readonly ?LoginUseCase $useCase = null,
+        private readonly ?SecurityInputFactory $inputFactory = null
     ) {}
 
     public function __invoke(Request $request, array $args = []): Response
     {
-        // Trick: bypass real use case and return fake data for development
-        // $input = $this->inputFactory->createLoginFromRequest($request);
-        // $output = $this->useCase->execute($input);
+        $input = $this->inputFactory->createLoginFromRequest($request);
+        $output = $this->useCase->execute($input);
 
-        $output = new LoginOutputDto(
-            tokenJwt: "npA",
-            loginId: "TESTCLIENTCOMPLET",
-            userName: "TEST CLIENT Complet",
-            email: "noreply@techem.fr",
-            userType: "C",
-            adresse: "",
-            cp: "",
-            ville: "",
-            phoneNumber: "",
-            firstName: "Client",
-            userRole: "",
-            clientName: "",
-            nbImmeubles: -1,
-            seuilConsoEf: -1,
-            seuilConsoEc: -1,
-            seuilConsoRepart: -1,
-            seuilConsoCet: -1,
-            seuilConsoActif: false,
-            seuilConsoEmail: "",
-            showImmeublesArc: true,
-            showFactures: true,
-            showChgtOccupant: true,
-            showChantiers: true
-        );
         return $this->responder->respond($output);
     }
 }
